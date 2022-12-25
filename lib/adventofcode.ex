71909a3cbad235388ef00da53902a0b8f4d6354e
Mike Rosengrant <mike@rosengrant.me>
2022-12-25 12:06:13 -0500

    Advent of Code Day 2 part 1

--- lib/adventofcode.ex
+++ lib/adventofcode.ex
@@ -52,4 +52,100 @@ defmodule AdventOfCode01 do
   end
 end
 
-AdventOfCode01.solve()
+defmodule AdventOfCode02 do
+  @doc ~S"""
+  --- Day 2: Rock paper scissors ---
+  Suppose you were given the following strategy guide:
+
+  A Y
+  B X
+  C Z
+
+  This strategy guide predicts and recommends the following:
+
+      In the first round, your opponent will choose Rock (A), and you should choose Paper (Y). This ends in a win for you with a score of 8 (2 because you chose Paper + 6 because you won).
+      In the second round, your opponent will choose Paper (B), and you should choose Rock (X). This ends in a loss for you with a score of 1 (1 + 0).
+      The third round is a draw with both players choosing Scissors, giving you a score of 3 + 3 = 6.
+
+  In this example, if you were to follow the strategy guide, you would get a total score of 15 (8 + 1 + 6).
+
+  What would your total score be if everything goes exactly according to your strategy guide?
+  """
+  @p1_rock "A"
+  @p1_paper "B"
+  @p1_scissors "C"
+
+  @p2_rock "X"
+  @p2_paper "Y"
+  @p2_scissors "Z"
+
+  @rock_val 1
+  @paper_val 2
+  @scissors_val 3
+
+  @win_val 6
+  @loss_val 0
+  @tie_val 3
+
+  def solve do
+    {:ok, contents} = File.read("lib/assets/adventofcode_02.txt")
+    # contents = "A Y\nB X\nC Z"
+
+    round_list =
+      contents
+      |> String.split("\n")
+      |> Enum.map(&String.split(&1, " "))
+      |> Enum.reject(&(&1 == [""]))
+
+    round_scores = Enum.map(round_list, &score_round(&1))
+    total_points = Enum.sum(round_scores)
+    IO.puts("SCORE: " <> to_string(total_points))
+  end
+
+  def score_round(pair_list) when pair_list == [@p1_rock, @p2_paper] do
+    # Paper covers rock, p2 wins
+    @paper_val + @win_val
+  end
+
+  def score_round(pair_list) when pair_list == [@p1_rock, @p2_scissors] do
+    # Rock breaks scissors, p2 loses
+    @scissors_val + @loss_val
+  end
+
+  def score_round(pair_list) when pair_list == [@p1_paper, @p2_rock] do
+    # Paper covers rock, p2 loses
+    @rock_val + @loss_val
+  end
+
+  def score_round(pair_list) when pair_list == [@p1_paper, @p2_scissors] do
+    # Scissors cut paper, p2 wins
+    @scissors_val + @win_val
+  end
+
+  def score_round(pair_list) when pair_list == [@p1_scissors, @p2_rock] do
+    # Rock breaks scissors, p2 wins
+    @rock_val + @win_val
+  end
+
+  def score_round(pair_list) when pair_list == [@p1_scissors, @p2_paper] do
+    # Scissors cut paper, p2 loses
+    @paper_val + @loss_val
+  end
+
+  def score_round(pair_list) when pair_list == [@p1_rock, @p2_rock] do
+    # both rock, draw
+    @rock_val + @tie_val
+  end
+
+  def score_round(pair_list) when pair_list == [@p1_paper, @p2_paper] do
+    # both paper, draw
+    @paper_val + @tie_val
+  end
+
+  def score_round(pair_list) when pair_list == [@p1_scissors, @p2_scissors] do
+    # both scissors, draw
+    @scissors_val + @tie_val
+  end
+end
+
+AdventOfCode02.solve()
